b831231631626d895eff52f449c1c7059b26ca9c
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2025-02-04 11:00:28 +0300

    BJS2-54869 - Url Shortener

--- src/main/java/faang/school/urlshortenerservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/urlshortenerservice/config/redis/RedisConfig.java
@@ -1,13 +1,13 @@
 package faang.school.urlshortenerservice.config.redis;
 
 import faang.school.urlshortenerservice.model.entity.Url;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -26,11 +26,11 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
+    public RedisTemplate<String, Url> redisTemplate() {
+        RedisTemplate<String, Url> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
 }
